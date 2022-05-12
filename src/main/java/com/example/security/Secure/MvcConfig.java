b2d8db9366399303b583272af67c9c69ea76dd33
Rajesh Kumar Sahoo <46929437+rajeshcts095@users.noreply.github.com>
2022-05-12 16:25:13 +0530

    Update MvcConfig.java

--- src/main/java/com/example/security/Secure/MvcConfig.java
+++ src/main/java/com/example/security/Secure/MvcConfig.java
@@ -1,5 +1,6 @@
 package com.example.security.Secure;
 
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
