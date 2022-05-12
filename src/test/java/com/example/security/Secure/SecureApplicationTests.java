cb7db4fc3e33301d041765453fca96a102e60834
Rajesh Kumar Sahoo <sahoo.rajesh2@gmail.com>
2022-05-12 15:59:10 +0530

    first commit

---
+++ src/test/java/com/example/security/Secure/SecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.security.Secure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
